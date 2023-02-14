7e1cf6b45e0d30ad45e533c49941ed94fe489794
Kail-Lee8282 <73209484+Kail-Lee8282@users.noreply.github.com>
2023-02-14 13:53:32 +0900

    delete build directory
    not required

--- build/test/test.resolvers.js
+++
@@ -1,9 +0,0 @@
-import { books } from "./test.dataset";
-// Resolvers define how to fetch the types defined in your schema.
-// This resolver retrieves books from the "books" array above.
-const resolvers = {
-    Query: {
-        books: () => books,
-    },
-};
-export default resolvers;
